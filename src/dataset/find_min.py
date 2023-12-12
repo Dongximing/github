d55253a04f09614dd47a0e3c92ff5613785ce70b
Dongximing <dongx34@myumanitoba.ca>
2023-12-11 21:20:12 -0500

    update max len

---
+++ src/dataset/find_min.py
@@ -0,0 +1,16 @@
+from datasets import Dataset
+from transformers import AutoTokenizer, pipeline
+gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2-large')
+ds = Dataset.from_file("dataset/train_dataset/dataset.arrow")
+
+def tokenize(sample):
+    # print(type(sample["prompt"]))
+    # print("prompt :", sample["prompt"]["text"])
+    sample["tokens"] = gpt2_tokenizer.encode(sample["prompt"]["text"])
+    return sample
+encoded_ds = ds.map(tokenize, batched=False)
+encoded_ds = encoded_ds.map(lambda sample: {"length": len(sample["tokens"])}, batched=False)
+
+# 找出最小长度
+min_length = min(encoded_ds['length'])
+print("最小编码长度:", min_length)
